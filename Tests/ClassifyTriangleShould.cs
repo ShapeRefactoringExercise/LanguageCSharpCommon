8503fe58014e59eb54a22f2c81b3fd7b40861eaf
Jason Kerney <Jason@TeamKerney.com>
2024-06-21 16:04:49 -0700

    tests around classifying a triangle

--- Tests/ClassifyTriangleShould.cs
+++ Tests/ClassifyTriangleShould.cs
@@ -21,32 +21,32 @@ namespace Shape.Tests
             Assert.AreEqual("Triangle", result.Type);
         }
 
-        // [TestMethod]
-        // public void ClassifyFourPointsWhereFirstAndLastAreDifferentAsOther()
-        // {
-        //     var points = Builder.Build(
-        //         (0, 0),
-        //         (0, 5),
-        //         (3, 5),
-        //         (0, 1)
-        //     );
-        //
-        //     var result = Classifier.Classify(points);
-        //     Assert.AreEqual("Other", result.Type);
-        // }
-        //
-        // [TestMethod]
-        // public void ClassifyFourPointsWhereFirstThreeAreNotUniqueAsOther()
-        // {
-        //     var points = Builder.Build(
-        //         (0, 0),
-        //         (0, 5),
-        //         (0, 5),
-        //         (0, 0)
-        //     );
-        //
-        //     var result = Classifier.Classify(points);
-        //     Assert.AreEqual("Other", result.Type);
-        // }
+        [TestMethod]
+        public void ClassifyFourPointsWhereFirstAndLastAreDifferentAsOther()
+        {
+            var points = Builder.Build(
+                (0, 0),
+                (0, 5),
+                (3, 5),
+                (0, 1)
+            );
+
+            var result = Classifier.Classify(points);
+            Assert.AreEqual("Other", result.Type);
+        }
+
+        [TestMethod]
+        public void ClassifyFourPointsWhereFirstThreeAreNotUniqueAsOther()
+        {
+            var points = Builder.Build(
+                (0, 0),
+                (0, 5),
+                (0, 5),
+                (0, 0)
+            );
+
+            var result = Classifier.Classify(points);
+            Assert.AreEqual("Other", result.Type);
+        }
     }
 }
