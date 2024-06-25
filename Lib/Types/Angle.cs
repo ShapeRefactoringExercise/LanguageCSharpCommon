407162c3397a3aaf6ecbcf5bace76261a9370642
Jason Kerney <Jason@TeamKerney.com>
2024-06-25 16:00:15 -0700

    triangles have angles

---
+++ Lib/Types/Angle.cs
@@ -0,0 +1,35 @@
+﻿namespace Shape.Lib.Types;
+
+public class Angle
+{
+    public Angle(Point p1, Point vertex, Point p2)
+    {
+        P1 = p1;
+        Vertex = vertex;
+        P2 = p2;
+
+        SideA = new LineSegment(P1, Vertex);
+        SideB = new LineSegment(P2, Vertex);
+
+        var sideC = new LineSegment(P1, P2);
+
+        var c = sideC.Length; // 3, 4, 5,
+        var a = SideA.Length; // 4, 5, 3,
+        var b = SideB.Length; // 5, 3, 4
+
+        var applesauce = (Math.Pow(a, 2) + Math.Pow(b, 2) - Math.Pow(c, 2));
+        var pearsauce = 2 * a * b;
+        var bananaCream = Math.Round(applesauce / pearsauce, 6);
+
+        Degrees = Math.Acos(bananaCream) * (180 /  Math.PI); // 36.87, 53.13, 90
+    }
+
+    public Point Vertex { get; }
+    public Point P1 { get; }
+    public Point P2 { get; }
+
+    public LineSegment SideA { get; }
+    public LineSegment SideB { get; }
+
+    public double Degrees { get; }
+}
--- Lib/Types/Triangle.cs
+++ Lib/Types/Triangle.cs
@@ -8,18 +8,25 @@ public class Triangle: IShape
         SideB = sideB;
         SideC = sideC;
 
-        P1 = SideA.P1;
-        P2 = SideB.P1;
-        P3 = SideC.P1;
+        PB = SideA.P1;
+        PC = SideB.P1;
+        PA = SideC.P1;
+
+        AngleA = new Angle(PC, PA, PB);
+        AngleB = new Angle(PA, PB, PC);
+        AngleC = new Angle(PB, PC, PA);
     }
 
-    public Point P1 { get; }
-    public Point P2 { get; }
-    public Point P3 { get; }
+    public Point PB { get; }
+    public Point PC { get; }
+    public Point PA { get; }
 
     public LineSegment SideA { get; }
     public LineSegment SideB { get; }
     public LineSegment SideC { get; }
 
+    public Angle AngleA { get; }
+    public Angle AngleB { get; }
+    public Angle AngleC { get; }
     public string Type => "Triangle";
 }
