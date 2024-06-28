ad72b9d649797452a1480d82cf090264e3971f74
Jason Kerney <Jason@TeamKerney.com>
2024-06-28 15:19:44 -0700

    starting to unwind math helper

--- Lib/MathHelper.cs
+++ Lib/MathHelper.cs
@@ -6,16 +6,6 @@ public static class MathHelper
 {
     public static bool IsEquivalentTo(this double? a, double? b)
     {
-        if (!a.HasValue && !b.HasValue)
-        {
-            return true;
-        }
-
-        if (!a.HasValue || !b.HasValue)
-        {
-            return false;
-        }
-
-        return Math.Abs(a.Value - b.Value) <= 0.001;
+        return Math.Abs(a.GetValueOrDefault() - b.GetValueOrDefault()) <= 0.001;
     }
 }
